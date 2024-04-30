41ccc15bb5f07c835306d48bafd6c74feabd8b23
Dávid Szépvölgyi <100283209+DaverSVK@users.noreply.github.com>
2024-04-30 13:39:10 +0200

    Create hodina_30_4_2024.py

---
+++ hodina_30_4_2024.py
@@ -0,0 +1,44 @@
+from random import randint
+
+#funkcia ktorá skontroluje či je platný vstup
+def overVstup(hracovTah,mozneTahy):
+    if hracovTah not in mozneTahy:
+        print("Zlý vstup")
+    return hracovTah in mozneTahy
+
+#funkcia, ktorá náhodne vykoná ťah oponenta
+def hernyTah(hracovTah):
+    vysledok = 0
+    mozneTahy = ["kamen","papier","noznice"]
+    if(overVstup(hracovTah.lower(),mozneTahy)):
+        moznost = randint(0,2)
+        tahOponenta = mozneTahy[moznost]
+        if hracovTah.lower()=="kamen" and tahOponenta.lower()=="papier":
+            print("Prehra O_O")
+            vysledok = -1
+        elif hracovTah.lower()=="papier" and tahOponenta.lower()=="noznice":
+            print("Prehra O_O")
+            vysledok = -1
+        elif hracovTah.lower()=="noznice" and tahOponenta.lower()=="kamen":
+            print("Prehra O_O")
+            vysledok = -1
+        elif hracovTah.lower()=="kamen" and tahOponenta.lower()=="noznice":
+            print("Výhra ^_^")
+            vysledok = 1
+        elif hracovTah.lower()=="papier" and tahOponenta.lower()=="kamen":
+            print("Výhra ^_^")
+            vysledok = 1
+        elif hracovTah.lower()=="noznice" and tahOponenta.lower()=="papier":
+            print("Výhra ^_^")
+            vysledok = 1
+        else:
+            print("Remíza -_-")
+            vysledok = 0
+    return vysledok
+
+
+#---Medzi toto dopisuj----------
+
+#-------------------------------
+
+
